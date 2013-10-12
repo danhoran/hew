67bb2fdd4a53a625aa1ebcc2c4d4118a3c5eb9bc
Dan Horan <dan.horan@me.com>
2013-10-12 13:54:32 +0200

    Added interpolation tests

--- test/log.spec.js
+++ test/log.spec.js
@@ -12,6 +12,24 @@
       var result = log.error('Test String');
       expect(result).toEqual('Test String');
     });
+
+    // Ensure strings are interpolated
+    it('should interpolate a string when parsed string type', function() {
+      var result = log.error('Test String #{}', '2');
+      expect(result).toEqual('Test String #2');
+    });
+
+    // Ensure number are interpolated
+    it('should interpolate a string when parsed number type', function() {
+      var result = log.error('Test String #{}', 2);
+      expect(result).toEqual('Test String #2');
+    });
+
+    // Ensure objects are not valid interpolation arguments
+    it('should throw an error when attempting to interpolate objects', function() {
+      var result = log.error('Test String #{}', {number: 3});
+      expect(result).toThrow();
+    });
   });
 
 }).call(this);
