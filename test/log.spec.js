b674c2686430c742212e39f85c3d52982979771a
davidreid <davidreiduni@gmail.com>
2013-10-12 17:59:40 +0100

    Updated the log spec tests

--- test/log.spec.js
+++ test/log.spec.js
@@ -10,12 +10,16 @@
     // Ensure a string is returned
     it('should return a string', function() {
       var result = log.error('Test String');
-      
+      expect(typeof(result)).toBe('string');
     });
 
     // Ensure the message contains a timestamp
     it('should contain a timestamp', function() {
+      var result = log.error('Test string'),
+            substring = result.substring(1, 10),
+            date = new Date( substring );
 
+      expect(date).not.toBe('Invalid Date');
     });
   });
 
